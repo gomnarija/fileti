ed12f8b0f8892a288c2d8f77770912ab1d3c3bbe
gomnarija <lazarkarasicevic@protonmail.com>
2021-07-04 00:40:38 +0200

    license :)

--- src/utils/io.c
+++ src/utils/io.c
@@ -1,3 +1,22 @@
+//   Copyright 2021 gomnarija 
+//
+//   This file is part of fileTI.
+//
+//   fileTI is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   fileTI is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with fileTI; if not see <http://www.gnu.org/licenses/>.
+//
+
+
 #include "io.h"
 
 
@@ -6,7 +25,6 @@ int io_write(const char * file_name,const char *buffer,int buff_size)
 {
 	FILE *fp = fopen(file_name,"a+");
 
-	printf("asd %d",buff_size);
 	if(!fp)
 		return -1;
 	if(fwrite(buffer,sizeof(char),buff_size,fp) <= 0)
--- src/utils/log.c
+++ src/utils/log.c
@@ -1,3 +1,22 @@
+//   Copyright 2021 gomnarija 
+//
+//   This file is part of fileTI.
+//
+//   fileTI is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   fileTI is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with fileTI; if not see <http://www.gnu.org/licenses/>.
+//
+
+
 #include "log.h"
 
 #include "stdio.h"
@@ -11,38 +30,44 @@ void log_log(int log_level,const char *message)
 	time_t t  = time(NULL);
 	struct tm log_time =  *localtime(&t);	
 
-	char lvl_msg[16];
+	char lvl_msg[16],time_msg[22],*msg;
+
 	switch(log_level)
 	{
 		case LOG_WARNING:
-			strcpy(lvl_msg,"[WARNING]");break;
+			snprintf(lvl_msg,10,"%s","[WARNING]");break;
 
 		case LOG_ERROR:
-			strcpy(lvl_msg,"[ERROR]");break;
+			snprintf(lvl_msg,8,"%s","[ERROR]");break;
 		default:
-			strcpy(lvl_msg,"[MESSAGE]");
+			snprintf(lvl_msg,10,"%s","[MESSAGE]");
 	}
-
-		
-	if((fp  = fopen(LOG_FILE,"a+")) != NULL)
-	{
-		if(fprintf(fp,"[%d-%02d-%02d %02d:%02d:%02d]", //current time
+	
+	snprintf(time_msg,22,"[%d-%02d-%02d %02d:%02d:%02d]", //current time
 						 	log_time.tm_year + 1900, 
 							log_time.tm_mon + 1, 
 							log_time.tm_mday, 
 							log_time.tm_hour, 
 							log_time.tm_min, 
-							log_time.tm_sec))
+							log_time.tm_sec);
 		
-	
-			if(fprintf(fp,"%s",lvl_msg))//log level
-				if(fprintf(fp,"%s",">"))//>
-					fprintf(fp,"%s",message);//message
-	
-	
-		fprintf(fp,"\n");//new line always
-		
+
+
+	if((msg = (char *)malloc(strlen(message)+strlen(lvl_msg)+strlen(time_msg)+3))==NULL)
+		return;
+
+
+	snprintf(msg,
+		strlen(message)+strlen(lvl_msg)+strlen(time_msg)+3,
+			"%s%s>%s\n",time_msg,lvl_msg,message);
+
+	if((fp  = fopen(LOG_FILE,"a+")) != NULL)
+	{
+			
+		fwrite(msg,sizeof(char),strlen(msg)+1,fp);
+
 		fclose(fp);		
 	}
+	free(msg);
 }
 
